9c97c093573b7e9a08725f2985aef3ee077df828
JasonLou99 <42426943+JasonLou99@users.noreply.github.com>
2022-05-18 14:59:14 +0800

    Update startClient.sh

--- scripts/startClient.sh
+++ scripts/startClient.sh
@@ -1,1 +1,1 @@
-go run /Causal_KV_Store/kvstore/kvclient/kvclient.go -cnums 1 -mode Request -onums 100 -servers 172.17.0.2:30011
+go run ../kvstore/kvclient/kvclient.go -cnums 1 -mode Request -onums 100 -servers 172.17.0.2:30011
