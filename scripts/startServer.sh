628e357897569667b4e1706aa86a6bfcb9948d87
JasonLou99 <42426943+JasonLou99@users.noreply.github.com>
2022-05-24 15:20:23 +0800

    Update startServer.sh

--- scripts/startServer.sh
+++ scripts/startServer.sh
@@ -1,1 +1,2 @@
-go run ../kvstore/kvserver/kvserver.go -address 172.17.0.2:3001 -members 172.17.0.2:3001
+go run ../kvstore/kvserver/kvserver.go -address 172.17.0.2:3001 -members 172.17.0.2:3001,172.17.0.3:3001,172.17.0.4:3001
+
